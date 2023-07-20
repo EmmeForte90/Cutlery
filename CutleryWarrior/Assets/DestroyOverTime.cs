5d4172450f5ffbb1240866c4e859025234a332aa
EmmeForte90 <mauroemme90@gmail.it>
2023-07-20 19:42:53 +0200

    Title

---
+++ CutleryWarrior/Assets/DestroyOverTime.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOverTime : MonoBehaviour
+{
+    public int lifeTime = 0;
+    public GameObject title;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       StartCoroutine(Destroy());
+    }
+
+    
+
+    IEnumerator Destroy()
+{   
+    yield return new WaitForSeconds(lifeTime);
+    title.gameObject.SetActive(false);
+    
+}
+}
