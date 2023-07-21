7edd96d4392a87bcc3d87324e72358a31792e8dd
EmmeForte90 <mauroemme90@gmail.it>
2023-07-21 09:58:18 +0200

    Fix

--- CutleryWarrior/Assets/Script/StartScene.cs
+++ CutleryWarrior/Assets/Script/StartScene.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class StartScene : MonoBehaviour
 {
@@ -8,7 +9,11 @@ public class StartScene : MonoBehaviour
     public GameObject FAct;
     public GameObject KAct;
     public GameObject SAct;    
-    public GameObject[] SpawnArr;    
+    public GameObject[] SpawnArr; 
+    public Collider[] BoxConfiner;    
+    private CinemachineConfiner confiner;
+    private CinemachineVirtualCamera vCam;
+
     private int IDPorta;
 public static StartScene instance;
 void Awake()
@@ -16,8 +21,11 @@ void Awake()
     if (instance == null){instance = this;}   
     if(!GameManager.instance.StartGame){   
     player = GameObject.FindWithTag("Player");
+    vCam = GameObject.FindWithTag("MainCamera").GetComponent<CinemachineVirtualCamera>(); //ottieni il riferimento alla virtual camera di Cinemachine
+    confiner = GameObject.FindWithTag("MainCamera").GetComponent<CinemachineConfiner>(); //ottieni il riferimento alla virtual camera di Cinemachine
     IDPorta = GameManager.instance.IDPorta;
     Spawn(IDPorta);
+    Confiner(IDPorta);
     CharacterMove.instance.inputCTR = false; 
     GameManager.instance.FadeOut();}
 }
@@ -28,6 +36,13 @@ void Awake()
         KAct.transform.position = SpawnArr[ID].transform.position;
         FAct.transform.position = SpawnArr[ID].transform.position;
         SAct.transform.position = SpawnArr[ID].transform.position;    
-        }
+    }
+
+    public void Confiner(int ID)
+    {
+        confiner.m_BoundingVolume  = null; 
+        confiner.m_BoundingVolume  = BoxConfiner[ID];       
+        vCam.Follow = player.transform;  
+    }
    
 }
