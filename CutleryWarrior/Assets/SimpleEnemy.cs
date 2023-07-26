5e20aaa1a7ae221150712558ee96f9c435277afc
EmmeForte90 <mauroemme90@gmail.it>
2023-07-26 19:59:32 +0200

    Battle test

--- CutleryWarrior/Assets/SimpleEnemy.cs
+++ CutleryWarrior/Assets/SimpleEnemy.cs
@@ -1,7 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Spine.Unity;
+using Spine;
 public class SimpleEnemy : MonoBehaviour
 {
    public float moveSpeed = 3f;
@@ -17,6 +18,26 @@ public class SimpleEnemy : MonoBehaviour
     
     public static SimpleEnemy instance;
 
+    [Header("Animations")]
+    [SpineAnimation][SerializeField] private string IdleAnimationName;
+    [SpineAnimation][SerializeField] private string WalkAnimationName;
+    [SpineAnimation][SerializeField] private string RunAnimationName;
+    [SpineAnimation][SerializeField] private string Atk1AnimationName;
+    [SpineAnimation][SerializeField] private string Atk2AnimationName;
+    [SpineAnimation][SerializeField] private string Atk3AnimationName;
+    [SpineAnimation][SerializeField] private string GuardAnimationName;
+    [SpineAnimation][SerializeField] private string GuardHitAnimationName;
+    [SpineAnimation][SerializeField] private string DodgeFAnimationName;
+    [SpineAnimation][SerializeField] private string DodgeBAnimationName;
+ // Nome dell'animazione di attacco base
+    //[SerializeField] private string attackAnimationName = "attack";
+
+    // Nome delle animazioni di combo (aggiunto a un numero per formare il nome completo)
+    private string currentAnimationName;
+    public SkeletonAnimation _skeletonAnimation;
+    public Spine.AnimationState _spineAnimationState;
+    public Spine.Skeleton _skeleton;
+    Spine.EventData eventData;
     private void Start()
     {
          if (instance == null)
@@ -24,25 +45,21 @@ public class SimpleEnemy : MonoBehaviour
             instance = this;
         }
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        _skeletonAnimation = GetComponent<SkeletonAnimation>();
+        if (_skeletonAnimation == null) {
+            Debug.LogError("Componente SkeletonAnimation non trovato!");
+        }        
+        _spineAnimationState = GetComponent<Spine.Unity.SkeletonAnimation>().AnimationState;
+        _spineAnimationState = _skeletonAnimation.AnimationState;
+        _skeleton = _skeletonAnimation.skeleton;    
     }
 
     private void Update()
     {
-        if(!DuelManager.instance.inputCTR){
+        if(!DuelManager.instance.inputCTR)
+        {FacePlayer();
         if (!isAttacking)
-        {
-            ChasePlayer();
-        } 
-
-
-
-        /*if (Input.GetKeyDown(KeyCode.Space))
-        {
-        Debug.Log("Cambio!");
-        player = GameObject.Find("F_Player").transform;
-
-        }   */
-        
+        {ChasePlayer();} 
         }
 
     }
@@ -57,7 +74,7 @@ public class SimpleEnemy : MonoBehaviour
         {
             // Movimento verso il player
             transform.position = Vector3.MoveTowards(transform.position, player.position, moveSpeed * Time.deltaTime);
-
+            Walk();
             // Se il nemico ha raggiunto il player, inizia l'attacco
             if (Vector3.Distance(transform.position, player.position) <= attackRange)
             {
@@ -69,6 +86,8 @@ public class SimpleEnemy : MonoBehaviour
     private void StartAttack()
     {
         isAttacking = true;
+        PlayAnimation(Atk1AnimationName); 
+        _spineAnimationState.Event += HandleEvent;
         // Ferma il nemico per attaccare
         // Inserisci qui l'animazione dell'attacco, se presente
         Debug.Log("Attacco!");
@@ -79,6 +98,7 @@ public class SimpleEnemy : MonoBehaviour
 
     private IEnumerator AttackPause()
     {
+        Idle();
         yield return new WaitForSeconds(attackPauseDuration);
         isAttacking = false;
     }
@@ -87,11 +107,76 @@ public class SimpleEnemy : MonoBehaviour
     {
         // Inserisci qui la logica per subire danni
     }
-
+    void FacePlayer()
+    {
+        
+            if (player.transform.position.x > transform.position.x)
+            {
+                transform.localScale = new Vector3(-1, 1, 1);
+            }
+            else if (player.transform.position.x < transform.position.x)
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+            }
+        
+    }
     private void Die()
     {
         // Inserisci qui la logica per la morte del nemico
         Debug.Log("Il nemico è morto!");
         Destroy(gameObject);
     }
+
+    private void PlayAnimation(string animationName)
+    {
+        _skeletonAnimation.state.SetAnimation(0, animationName, false);
+        _skeletonAnimation.state.GetCurrent(0).Complete += OnAttackAnimationComplete;
+    }
+    public void Walk()
+{
+    if (currentAnimationName != WalkAnimationName)
+                {
+                    _spineAnimationState.SetAnimation(0, WalkAnimationName, true);
+                    currentAnimationName = WalkAnimationName;
+                     _spineAnimationState.Event += HandleEvent;
+                }
+
+}
+ public void Running()
+{
+    if (currentAnimationName != RunAnimationName)
+                {
+                    _spineAnimationState.SetAnimation(0, RunAnimationName, true);
+                    currentAnimationName = RunAnimationName;
+                     _spineAnimationState.Event += HandleEvent;
+                }
+
+}
+
+ public void Idle()
+{
+    if (currentAnimationName != IdleAnimationName)
+                {
+                    _spineAnimationState.SetAnimation(0, IdleAnimationName, true);
+                    currentAnimationName = IdleAnimationName;
+                     _spineAnimationState.Event += HandleEvent;
+                }
+
+}
+
+    private void OnAttackAnimationComplete(Spine.TrackEntry trackEntry)
+{
+    // Remove the event listener
+    trackEntry.Complete -= OnAttackAnimationComplete;
+
+    // Clear the track 1 and reset to the idle animation
+    _skeletonAnimation.state.ClearTrack(1);
+    _skeletonAnimation.state.SetAnimation(0, IdleAnimationName, true);
+}
+void HandleEvent (TrackEntry trackEntry, Spine.Event e) 
+{
+//Normal VFX
+    if (e.Data.Name == "slashV") 
+    {}
+}
 }
