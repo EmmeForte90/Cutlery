dbe0d08bffed6ea74cc9afcd03cb37b6ba67cb58
EmmeForte90 <mauroemme90@gmail.it>
2023-11-26 11:49:09 +0100

    Fix

--- CutleryWarrior/Assets/Script/Minimap.cs
+++ CutleryWarrior/Assets/Script/Minimap.cs
@@ -1,38 +1,36 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Minimap : MonoBehaviour {
 
 	public GameObject player;
-    public SwitchCharacter rotationSwitcher;
+    //public SwitchCharacter rotationSwitcher;
 	public GameObject ForkActive;
 	public GameObject SpoonActive;
 	public GameObject KnifeActive;
-	public void Awake(){StartCoroutine(CoordinateActor());}
-	public void LateUpdate()
-	{
-		switch(rotationSwitcher.ConInt)
-        {
-            case 1:
-			player.transform.position = ForkActive.transform.position;
+	public GameManager GM;
+    public Camera unityCamera; // La camera di Unity
+	public void Update()
+{
+    switch (GM.CharacterID)
+    {
+        case 1:
+            player.transform.position = ForkActive.transform.position;
+            unityCamera.transform.position = new Vector3(player.transform.position.x, unityCamera.transform.position.y, player.transform.position.z);
             break;
-            case 2:
-			player.transform.position = KnifeActive.transform.position;
+        case 2:
+            player.transform.position = KnifeActive.transform.position;
+            unityCamera.transform.position = new Vector3(player.transform.position.x, unityCamera.transform.position.y, player.transform.position.z);
             break;
-            case 3:
-			player.transform.position = SpoonActive.transform.position;
+        case 3:
+            player.transform.position = SpoonActive.transform.position;
+            unityCamera.transform.position = new Vector3(player.transform.position.x, unityCamera.transform.position.y, player.transform.position.z);
             break;
-        }
-		Vector3 newPosition = player.transform.position;
-		newPosition.y = transform.position.y;
-		transform.position = newPosition;
-		//transform.rotation = Quaternion.Euler(90f, player.transform.eulerAngles.y, 0f);
-	}
+    }
 
-	IEnumerator CoordinateActor()
-    {  yield return new WaitForSeconds(0.2f);
-	player.transform.position = ForkActive.transform.position;
-	}
+    Vector3 newPosition = player.transform.position;
+    newPosition.y = transform.position.y;
+    transform.position = newPosition;
+    //transform.rotation = Quaternion.Euler(90f, player.transform.eulerAngles.y, 0f);
+}
 }
